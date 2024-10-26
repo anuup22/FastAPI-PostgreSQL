a2303c5ae97029c2bb264ac2e894cd83af71615d
Anup Gupta <anuup22@gmail.com>
2024-10-26 23:13:02 +0530

    Add database configuration and connection setup

---
+++ database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# this should be in a .env file
+URL_DATABASE = "postgresql://myroot:mypassword@localhost:5432/QuizApplication"
+
+# engine handles the connection with the database
+engine = create_engine(URL_DATABASE)
+
+# SessionLocal handles the transactions with the database
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base handles the models of the database
+Base = declarative_base()
