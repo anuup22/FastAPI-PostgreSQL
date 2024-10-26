d1b0dc34272d1df687bb83b2af07b28ab6c086ed
Anup Gupta <anuup22@gmail.com>
2024-10-26 23:34:54 +0530

    Update database URL to lowercase

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # this should be in a .env file
-URL_DATABASE = "postgresql://myroot:mypassword@localhost:5432/QuizApplication"
+URL_DATABASE = "postgresql://myroot:mypassword@localhost:5432/quizapplication"
 
 # engine handles the connection with the database
 engine = create_engine(URL_DATABASE)
