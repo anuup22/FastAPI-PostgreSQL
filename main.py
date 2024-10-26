4118fcb6c0d48c84cad0be90ce51d873e18a22ba
Anup Gupta <anuup22@gmail.com>
2024-10-27 00:23:24 +0530

    Refactor code to use keyword arguments in create_question function

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ async def create_question(question: QuestionBase, db: db_dependency):
     db.refresh(db_question)
 
     for choice in question.choices:
-        db_choice = models.Choices(choice.choice_text, is_correct=choice.is_correct, question_id=db_question.id)
+        db_choice = models.Choices(choice_text=choice.choice_text, is_correct=choice.is_correct, question_id=db_question.id)
         db.add(db_choice)
     db.commit()
 
@@ -48,16 +48,13 @@ async def create_question(question: QuestionBase, db: db_dependency):
 async def get_question(question_id: int, db: db_dependency):
     return db.query(models.Questions).filter(models.Questions.id == question_id).first()
 
-@app.get("choices/{question_id}")
-async def get_choices(question_id: int, db: db_dependency):
-    result = db.query(models.Choices).filter(models.Choices.question_id == question_id).all()
-    if not result:
-        raise HTTPException(status_code=404, detail="Choices not found")
-    return result
-
 @app.get("/questions/")
 async def get_questions(db: db_dependency):
     return db.query(models.Questions).all()
+
+@app.get("choices/{question_id}")
+async def get_choices(question_id: int, db: db_dependency):
+    return db.query(models.Choices).filter(models.Choices.question_id == question_id).all()
 
 @app.delete("/questions/{question_id}")
 async def delete_question(question_id: int, db: db_dependency):
--- models.py
+++ models.py
@@ -14,3 +14,8 @@ class Choices(Base):
     choice_text = Column(String, index=True)
     is_correct = Column(Boolean, default=False)
     question_id = Column(Integer, ForeignKey("questions.id"))
+
+    def __init__(self, choice_text, is_correct, question_id):
+        self.choice_text = choice_text
+        self.is_correct = is_correct
+        self.question_id = question_id
